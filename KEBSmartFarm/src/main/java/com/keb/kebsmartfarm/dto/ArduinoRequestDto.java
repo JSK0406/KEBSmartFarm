584e7baab154f97451e08b9b37f2013b60c6b8f4
JGM <cnxw4570123@gmail.com>
2023-08-13 22:43:40 +0900

    Fix : Fixed ArduinoRequestDto to have userSeqNum     ArduinoKit의 연관관계를 User에서 UserSeqNum을 직접 갖게 바꾸다보니 기존 User를 생성하고 넣어주는 코드가 더이상 제대로 동작하지 않는 것을 확인함.     userSeqNum을 갖도록 수정하였음.

--- KEBSmartFarm/src/main/java/com/keb/kebsmartfarm/dto/ArduinoRequestDto.java
+++ KEBSmartFarm/src/main/java/com/keb/kebsmartfarm/dto/ArduinoRequestDto.java
@@ -19,14 +19,13 @@ import java.util.Date;
 public class ArduinoRequestDto {
     private Long userSeqNum;
     private String serialNum;
-    private Date date;
     private String deviceName;
     public ArduinoKit toArduinoKit() {
         return ArduinoKit.builder()
                 .serialNum(getSerialNum())
                 // 문제
-                .date(date)
-                .user(User.builder().userSeqNum(SecurityUtil.getCurrentUserId()).build())
+                .date(new Date())
+                .userSeqNum(SecurityUtil.getCurrentUserId())
                 .deviceName(getDeviceName())
                 // 일단 임시로 NPE 생기지 않도록 조치함
                 .PlantList(new ArrayList<>())
