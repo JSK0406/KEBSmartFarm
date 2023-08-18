9fd9765af0e206c4f79052108b0c5b88c318ed99
JGM <cnxw4570123@gmail.com>
2023-08-18 10:47:56 +0900

    Refactor : Separate UserController's kit relative codes into KitController UserController에서 하는 일이 너무 많다고 판단하여 KitController를 생성해 UserController는 유저와 관련된 일을, 키트는 KitController에서 처리하도록 수정함.

---
+++ KEBSmartFarm/src/main/java/com/keb/kebsmartfarm/Controller/KitController.java
@@ -0,0 +1,57 @@
+package com.keb.kebsmartfarm.Controller;
+
+import com.keb.kebsmartfarm.dto.*;
+import com.keb.kebsmartfarm.service.KitAndPlantManageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RequestMapping("/kit")
+@RestController
+@RequiredArgsConstructor
+public class KitController {
+
+    private final KitAndPlantManageService kitAndPlantManageService;
+
+    @PostMapping
+    public ResponseEntity<ArduinoResponseDto> addKit(@RequestBody ArduinoRequestDto requestDto) {
+        return ResponseEntity.ok(kitAndPlantManageService.addKit(requestDto));
+    }
+
+    @DeleteMapping("/{kitNo}")
+    public ResponseEntity<?> deleteKit(@PathVariable long kitNo) {
+        kitAndPlantManageService.deleteKit(kitNo);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+
+    @PostMapping("/{kitNo}/plant")
+    public ResponseEntity<PlantResponseDto> addPlantToKit(@PathVariable long kitNo, @RequestBody PlantRequestDto plantRequestDto) {
+        return ResponseEntity.ok(kitAndPlantManageService.plantingPlant(kitNo, plantRequestDto));
+    }
+
+    @PostMapping("/{kitNo}/growth")
+    public ResponseEntity<PreviousPlantDto> moveToPreviousPlant(@PathVariable long kitNo) {
+        return ResponseEntity.ok(kitAndPlantManageService.completingPlantGrowth(kitNo));
+    }
+
+    @DeleteMapping("/{kitNo}/plant")
+    public ResponseEntity<?> deletePlant(@PathVariable long kitNo) {
+        kitAndPlantManageService.deletingPlant(kitNo);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @GetMapping("/plantList")
+    public ResponseEntity<Map<String, Object>> getUserPlantList() {
+        return ResponseEntity.ok(kitAndPlantManageService.gettingListOfUsersPlant());
+    }
+
+    @GetMapping("/{kitNo}/light")
+    public ResponseEntity<Boolean> lightKit(@PathVariable long kitNo){
+        return ResponseEntity.ok(kitAndPlantManageService.controlLight(kitNo));
+    }
+
+}
--- KEBSmartFarm/src/main/java/com/keb/kebsmartfarm/Controller/UserController.java
+++ KEBSmartFarm/src/main/java/com/keb/kebsmartfarm/Controller/UserController.java
@@ -1,14 +1,12 @@
 package com.keb.kebsmartfarm.Controller;
 
-import com.keb.kebsmartfarm.dto.*;
-import com.keb.kebsmartfarm.service.KitAndPlantManageService;
+import com.keb.kebsmartfarm.dto.ChangePasswordDto;
+import com.keb.kebsmartfarm.dto.UserRequestDto;
+import com.keb.kebsmartfarm.dto.UserResponseDto;
 import com.keb.kebsmartfarm.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
@@ -16,8 +14,6 @@ import java.util.Map;
 public class UserController {
 
     private final UserService userService;
-
-    private final KitAndPlantManageService kitAndPlantManageService;
 
     @GetMapping("/me")
     public ResponseEntity<UserResponseDto> getMyUserInfo() {
@@ -34,45 +30,6 @@ public class UserController {
     public ResponseEntity<UserResponseDto> setUserPassword(@RequestBody ChangePasswordDto changePasswordDto) {
         return ResponseEntity.ok(userService.changeUserPassword(changePasswordDto.getExPassword(), changePasswordDto.getNewPassword()));
     }
-
-    @PostMapping("/kit")
-    public ResponseEntity<ArduinoResponseDto> addKit(@RequestBody ArduinoRequestDto requestDto) {
-        return ResponseEntity.ok(kitAndPlantManageService.addKit(requestDto));
-    }
-
-    @DeleteMapping("/kit/{kitNo}")
-    public ResponseEntity<?> deleteKit(@PathVariable long kitNo) {
-        kitAndPlantManageService.deleteKit(kitNo);
-        return ResponseEntity.status(HttpStatus.OK).build();
-    }
-
-
-    @PostMapping("/kit/{kitNo}/plant")
-    public ResponseEntity<PlantResponseDto> addPlantToKit(@PathVariable long kitNo, @RequestBody PlantRequestDto plantRequestDto) {
-        return ResponseEntity.ok(kitAndPlantManageService.plantingPlant(kitNo, plantRequestDto));
-    }
-
-    @PostMapping("/kit/{kitNo}/growth")
-    public ResponseEntity<PreviousPlantDto> moveToPreviousPlant(@PathVariable long kitNo) {
-        return ResponseEntity.ok(kitAndPlantManageService.completingPlantGrowth(kitNo));
-    }
-
-    @DeleteMapping("/kit/{kitNo}/plant")
-    public ResponseEntity<?> deletePlant(@PathVariable long kitNo) {
-        kitAndPlantManageService.deletingPlant(kitNo);
-        return ResponseEntity.status(HttpStatus.OK).build();
-    }
-
-    @GetMapping("/plantList")
-    public ResponseEntity<Map<String, Object>> getUserPlantList() {
-        return ResponseEntity.ok(kitAndPlantManageService.gettingListOfUsersPlant());
-    }
-
-    @GetMapping("/kit/{kitNo}/light")
-    public ResponseEntity<Boolean> lightKit(@PathVariable long kitNo){
-        return ResponseEntity.ok(kitAndPlantManageService.controlLight(kitNo));
-    }
-
 }
 
 
