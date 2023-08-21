7e7bd17df7b6527689b9bfd54f2c48d86339e5b5
JGM <cnxw4570123@gmail.com>
2023-08-21 14:36:06 +0900

    Fix : Fix UserFindId

--- KEBSmartFarm/src/main/java/com/keb/kebsmartfarm/service/AuthService.java
+++ KEBSmartFarm/src/main/java/com/keb/kebsmartfarm/service/AuthService.java
@@ -8,6 +8,7 @@ import com.keb.kebsmartfarm.jwt.TokenProvider;
 import com.keb.kebsmartfarm.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
@@ -17,6 +18,7 @@ import org.springframework.stereotype.Service;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -50,7 +52,7 @@ public class AuthService {
     public Map<String, String> findIdByNameAndEmail(String userEmail, String userName) {
         Map<String, String> ret = new HashMap<>();
         User user = userRepository.findByUserEmail(userEmail).orElseThrow(() -> new RuntimeException(userEmail + "에 해당하는 회원이 없습니다."));
-        if(!user.getUserName().equalsIgnoreCase(userName)){
+        if(user.getUserName().equalsIgnoreCase(userName)){
             ret.put("userId", user.getUserId());
         }
         return ret;
